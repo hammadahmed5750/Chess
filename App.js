69a51342c164b0ecddb2bdabe935391c2dd6319c
hammadahmed5750 <68161673+hammadahmed5750@users.noreply.github.com>
2022-12-01 19:16:25 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import GameScreen from "./screen/GameScreen";
+import HomeScreen from "./screen/HomeScreen";
+
+export default function App() {
+  return (
+    <>
+      {/* <HomeScreen /> */}
+      <GameScreen />
+    </>
+  );
+}
